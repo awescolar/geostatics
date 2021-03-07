61edcf0c0dc44a550225df7f708af7b1a2dab064
Jady Lima <jadylsilva@gmail.com>
2021-03-07 11:16:33 -0300

    Rotas e banco de dados
    Atualização do banco de dados e das rotas.

---
+++ core/NotORM/PDOConf.php
@@ -0,0 +1,27 @@
+<?php
+    class PDOConf extends PDO{
+        private $engine = "mysql";
+        private $host = "127.0.0.1";
+        private $database = "geostatics";
+        private $user = "root";
+        private $pass = "";
+
+        static private $instance = null;
+
+        static public function getInstance(){
+            if(null === self::$instance){
+                $cl = __CLASS__;
+                self::$instance = new $cl;
+            }
+            return self::$instance;
+        }
+
+        public function __construct(){
+            $dns = $this->engire . ':dbname=' . $this->database . ";host=" . $this->host;
+            parent::__construct($dns, $this->user, $this->pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+        }
+
+        private function __clone(){}
+    }
+
+?>
