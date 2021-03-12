d5d480cd0415505a650b2033bc0344eb0a5671f7
Jady Lima <jadylsilva@gmail.com>
2021-03-12 17:48:26 -0300

    Alteração de dados do banco

--- core/NotORM/PDOConfig.php
+++ core/NotORM/PDOConfig.php
@@ -6,7 +6,7 @@
       private $host = "127.0.0.1";
       private $database = "geostatics";
       private $user = "root";
-      private $pass = "admin";
+      private $pass = "";
 
 
         static private $instance = null;
@@ -20,7 +20,7 @@
         }
 
         public function __construct(){
-            $dns = $this->engine . ':dbname=' . $this->database . ";host=" . $this->host;
+            $dns = $this->engine . ':host=' . $this->host . ";dbname=" . $this->database;
             parent::__construct($dns, $this->user, $this->pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
             //parent::setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             //parent::setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
